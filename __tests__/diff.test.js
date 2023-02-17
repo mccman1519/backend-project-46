c30b4bb1e8e0c04fb689699bb58ca76ab158bc25
mccman1519 <vt.banzaj@gmail.com>
2023-02-17 22:56:58 +0300

    Fix diff.js second try

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -3,7 +3,6 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import expectation from '../__fixtures__/expectation.js';
 import diff from '../src/diff.js';
-import formatter from '../src/formatters/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,28 +14,30 @@ const fpCommon = `${__dirname}/../___fixtures_/common/`;
 describe('Test JSON', () => {
   const path1 = `${fpJSON}file1.json`;
   const path2 = `${fpJSON}file2.json`;
+
   test('stylish', () => {
-    expect(`${formatter.stylish(diff(path1, path2))}`).toEqual(expectation.stylish);
+    expect(`${diff(path1, path2, 'stylish')}`).toEqual(expectation.stylish);
   });
   test('plain', () => {
-    expect(`${formatter.plain(diff(path1, path2))}`).toEqual(expectation.plain);
+    expect(`${diff(path1, path2, 'plain')}`).toEqual(expectation.plain);
   });
   test('json', () => {
-    expect(`${formatter.json(diff(path1, path2))}`).toEqual(expectation.json);
+    expect(`${diff(path1, path2, 'json')}`).toEqual(expectation.json);
   });
 });
 
 describe('Test YAML', () => {
   const path1 = `${fpYAML}file1.yaml`;
   const path2 = `${fpYAML}file2.yml`;
+
   test('stylish', () => {
-    expect(`${formatter.stylish(diff(path1, path2))}`).toEqual(expectation.stylish);
+    expect(`${diff(path1, path2, 'stylish')}`).toEqual(expectation.stylish);
   });
   test('plain', () => {
-    expect(`${formatter.plain(diff(path1, path2))}`).toEqual(expectation.plain);
+    expect(`${diff(path1, path2, 'plain')}`).toEqual(expectation.plain);
   });
   test('json', () => {
-    expect(`${formatter.json(diff(path1, path2))}`).toEqual(expectation.json);
+    expect(`${diff(path1, path2, 'json')}`).toEqual(expectation.json);
   });
 });
 
@@ -44,11 +45,11 @@ describe('Test common', () => {
   test('File types', () => {
     const path1 = `${fpCommon}file.abc`;
     const path2 = `${fpJSON}file1.json`;
-    expect(() => `${formatter.stylish(diff(path1, path2))}`).toThrow('Cannot compare two different types of files');
+    expect(() => `${diff(path1, path2, 'stylish')}`).toThrow('Cannot compare two different types of files');
   });
 
   test('Unsupported mode', () => {
     const filePath = `${fpCommon}file.abc`;
-    expect(() => `${formatter.stylish(diff(filePath, filePath))}`).toThrow('Unsupported file type');
+    expect(() => `${diff(filePath, filePath, 'stylish')}`).toThrow('Unsupported file type');
   });
 });
